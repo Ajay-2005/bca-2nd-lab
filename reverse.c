cec4724dfc65d660780a9ba6e1c713d85fee446d
Ajay <aji478514@gmail.com>
2023-09-18 20:43:53 +0530

    the reverse of a sring using pointer program added

---
+++ reverse.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char str[50];
+    char *start;
+    char *end;
+    char temp;
+    printf("Enter a string:\n");
+    gets(str);
+    start=str;
+    end=str+strlen(str)-1;
+    while(start<end){
+       temp=*start;
+       *start=*end;
+       *end=temp;
+        start++;
+        end--;
+    }
+    printf("Reverse string is %s",str);
+    return 0;
+}
