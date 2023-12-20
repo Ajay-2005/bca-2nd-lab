a5bf7c6669a32a7e647d8849ba07611f8eae35b2
Ajay <aji478514@gmail.com>
2023-12-20 06:50:20 +0530

    Delete a node in given list

---
+++ deletion_linkedlist.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>  
+    printf("The elements of the linked list: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct node *head = NULL;
+    struct node *newNode = NULL;
+    struct node *temp = NULL;
+    int n, i, pos;
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        newNode = (struct node*)malloc(sizeof(struct node));
+
+        if (newNode == NULL) {
+            printf("Memory allocation failed.\n");
+            return 1;
+        }
+
+        printf("Enter data: ");
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    traverselist(head);
+
+    printf("Enter position of data to remove: ");
+    scanf("%d", &pos);
+
+   
+    if (pos == 1) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    } else {
+        temp = head;
+        i = 1;
+        while (temp != NULL && i < pos - 1) {
+            temp = temp->next;
+            i++;
+        }
+
+        struct node *nextNode = temp->next->next;
+        free(temp->next);
+        temp->next = nextNode;
+    }
+
+    traverselist(head);
+
+
+    temp = head;
+    while (temp != NULL) {
+        struct node *nextNode = temp->next;
+        free(temp);
+        temp = nextNode;
+    }
+
+    return 0;
+}
+
